9f405a3477a41d491c75b0b934b99157abe42702
Sally N Fields <sally.nasr@gmail.com>
2017-06-01 21:17:44 -0500

    updating ajax logic

--- tableAjaxPost.js
+++ tableAjaxPost.js
@@ -1,34 +1,85 @@
-$.ajax({
-  url: "/tables",
-  type: "POST",
-  dataType: "json",
-  data: { objectData: someObject },
-  contentType: "application/json",
-  cache: false,
-  timeout: 5000,
-  complete: function () {
-    //called when complete
-    console.log('process complete');
-  },
 
-  success: function (data) {
+$(document).ready(function () {
 
-    if res.length < 5 {
+  // Here we use jQuery to select the header with "click-me" as its ID.
+  // Whenever it is clicked...
+  $("#click-me").on("click", function () {
 
-      for (i = 0, i < 5, i++){
-        $(".current_reservation").html((i + 1) + ": " + res[i].name);
-      }
-    }
-    else {
-      for (i = 5, i < res.length, i++){
-        $(".waiting_list").html((i + 1) + ": " + res[i].name);
-      }
+    var reservationCustomers = [];
+    var name = $
+    var phoneNumber = $
+    var email = $
+    var alias = $
 
+    var reservationCustomer = {
+      name: name,
+      phoneNumber: phoneNumber,
+      email: email,
+      alias: alias
     }
 
-  },
+    $.ajax({
+      url: "/tables",
+      type: "POST",
+      dataType: "json",
+      data: { objectData: someObject },
+      contentType: "application/json",
+      cache: false,
+      timeout: 5000,
+      complete: function () {
+        //called when complete
+        console.log('process complete');
+      },
 
-  error: function () {
-    console.log('process error');
-  },
-});
+      success: function (data) {
+        reservationCustomers.push(revervationCustomer);
+      },
+
+      error: function () {
+        console.log('process error');
+      },
+    });
+
+
+  }
+
+    $("#click-me").on("click", function () {   
+
+    $.ajax({
+        url: "/tables",
+        type: "GET",
+        dataType: "json",
+        data: { objectData: someObject },
+        contentType: "application/json",
+        cache: false,
+        timeout: 5000,
+        complete: function () {
+          //called when complete
+          console.log('process complete');
+        },
+
+        success: function (data) {
+
+          for (i = 0; i < 5; i++) {
+            $(".current_reservation").html((i + 1) + ": " + data[i].name);
+          }
+
+
+          for (i = 5; i < data.length; i++) {
+            $(".waiting_list").html((i + 1) + ": " + data[i].name);
+
+          }
+
+        },
+
+        error: function () {
+          console.log('process error');
+        },
+      });
+
+
+}
+
+
+
+
