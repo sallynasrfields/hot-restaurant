3c55177bbdbce375361433a5ea534a0154944829
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-06-01 21:49:25 -0500

    put in the routes for all html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,14 @@
-module.exports = function (app) {
-    console.log('it is here');
+var path = require('path');
 
+module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../home.html"));
     });
-
+    app.get('/reserve', function (app) {
+       res.sendFile(path.join(__dirname, '../reservation.html'))
+    });
+    app.get('/tables', function (app) {
+      res.sendFile(path.join(__dirname, '../tables.html'))
+    });
 };
 
